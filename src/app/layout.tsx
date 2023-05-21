fb766fce358c590199545358a39b1ab1cd634c1c
VGPABM <vincentiusgt23@gmail.com>
2023-05-21 23:10:01 +0700

    Added Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,12 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css';
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'MAGE 9 ITS',
   description: 'Lorem Ipsum Dolor Sit Amet',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-        {children}
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="en">{children}</html>;
 }
