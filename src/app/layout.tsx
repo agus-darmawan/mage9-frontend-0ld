0a89d4ad393f4c2e5f14ae54cd6c9531774ab8a1
agus-darmawan <5024211070@mhs.its.ac.id>
2023-05-21 23:02:26 +0700

    fix image using next image for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
+import logoMage from '../assets/images/logo.svg';
 
 export const metadata = {
   title: 'MAGE 9 ITS',
   description: 'Lorem Ipsum Dolor Sit Amet',
+  icons: {
+    icon: {
+      url: {logoMage},
+      type: "image/svg",
+    },
+    shortcut: { url: "../assets/images/logo.svg", type: "image/svg" },
+  },
 }
 
 export default function RootLayout({
--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
+import logoITS from '../assets/images/sponsors/its.png';
+import logoDPTSI from '../assets/images/sponsors/dptsi.png';
+import logoCe from '../assets/images/sponsors/ce.png';
+import logoHima from '../assets/images/sponsors/hima.svg';
+import logoApi from '../assets/images/sponsors/api.png';
+import logoAir from '../assets/images/sponsors/air.png';
 
 export default function Sponsors() {
   return (
@@ -9,35 +16,53 @@ export default function Sponsors() {
             <h1 className="focus:outline-none xl:text-4xl text-3xl text-center text-gray-800 dark:text-blue-400 font-extrabold pt-4">Media Partner</h1>
             </div>
             <div className="sm:py-6 px-8 sm:24 flex flex-wrap items-center">
-            <div
-                className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
-                <img className="focus:outline-none w-12 sm:w-16 " src="https://cdn-icons-png.flaticon.com/128/3800/3800024.png"
-                alt="Adidas" role="img" />
+            <div className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+                <Image className="focus:outline-none w-12 sm:w-16 " 
+                       src={logoITS}
+                       alt="ITS"
+                       width={100}
+                       height={100}
+                />
             </div>
-            <div
-                className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl ">
-                <img className="focus:outline-none w-12 sm:w-16" src="https://cdn-icons-png.flaticon.com/128/187/187902.png"
-                alt="Chanel" role="img" />
+            <div className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl ">
+                <Image className="focus:outline-none w-12 sm:w-16" 
+                       src={logoDPTSI}
+                       alt="DPTSI ITS"
+                       width={100}
+                       height={100}
+                />
             </div>
-            <div
-                className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
-                <img className="focus:outline-none w-12 sm:w-16" src="https://cdn-icons-png.flaticon.com/128/3845/3845877.png"
-                alt="Nike" role="img" />
+            <div className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+                <Image className="focus:outline-none w-12 sm:w-16" 
+                       src={logoCe}
+                       alt="CE ITS"
+                       width={100}
+                       height={100}
+                />
             </div>
-            <div
-                className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
-                <img className="focus:outline-none w-12 sm:w-16" src="https://cdn-icons-png.flaticon.com/128/732/732242.png"
-                alt="Toyota" role="img" />
+            <div className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+                <Image className="focus:outline-none w-12 sm:w-16" 
+                       src={logoHima}
+                       alt="HIMATEKOM"
+                       width={100}
+                       height={100}
+                />
             </div>
-            <div
-                className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
-                <img className="focus:outline-none w-12 sm:w-16" src="https://cdn-icons-png.flaticon.com/128/187/187868.png"
-                alt="Toyota" role="img" />
+            <div className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+                <Image className="focus:outline-none w-12 sm:w-16" 
+                       src={logoApi}
+                       alt="BARUNASTRA ITS"
+                       width={100}
+                       height={100}
+                />
             </div>
-            <div
-                className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
-                <img className="focus:outline-none w-12 sm:w-16" src="https://cdn-icons-png.flaticon.com/128/685/685680.png"
-                alt="Toyota" role="img" />
+            <div className="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+                <Image className="focus:outline-none w-12 sm:w-16" 
+                       src={logoAir}
+                       alt="BAYUCARAKA"
+                       width={100}
+                       height={100}
+                />
             </div>
             </div>
         </div>
