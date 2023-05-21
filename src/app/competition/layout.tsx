ed103db846273d1991f41d2eba61f92b8b21602a
agus-darmawan <5024211070@mhs.its.ac.id>
2023-05-21 19:19:40 +0700

    fix bug

--- src/app/competition/layout.tsx
+++ src/app/competition/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import HeroCompetition from '@/components/HeroCompetition'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
         <Navbar />
+        <HeroCompetition />
         {children}
         <Footer />
     </html>
