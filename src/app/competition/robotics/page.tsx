69a2caf6ce898061c0b883bc8ac27480a7d34968
agus-darmawan <5024211070@mhs.its.ac.id>
2023-05-28 23:04:07 +0700

    fix error import card in robotic page

--- src/app/competition/robotics/page.tsx
+++ src/app/competition/robotics/page.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Image from 'next/legacy/image';
-import Card from '@/components/Card';
 
-import blue from '../../../assets/images/bg/blue.svg';
 import RoboticContent from '@/components/RoboticContent';
 
 export default function page() {
