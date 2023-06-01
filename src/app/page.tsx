a43d36f79f9b4248db7381ee28a5528a56cbd0e0
agus-darmawan <5024211070@mhs.its.ac.id>
2023-06-01 09:34:58 +0700

    fix: fix navbar dropdown icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import About from '@/components/About';
 import Testimonials from '@/components/Testimonials';
 import Footer from '@/components/Footer';
 import Support from '@/components/Support';
+import Example from '@/components/Carousel';
 
 
 
@@ -13,13 +14,14 @@ import Support from '@/components/Support';
 export default function Home() {
   return (
     <body className=" bg-winterColor1 font-sans md:bg-top bg-center">
-      <div className="min-h-screen bg-Hero1 bg-cover bg-repeat ">
+      <div className="min-h-screen bg-Hero bg-cover bg-repeat ">
         <Navbar />
         <MainHero />
       </div>
       <div className="">
         <About />
         <Testimonials />
+        <Example />
         <Support />     
          <Footer/>
 
